c750015a1a2ada04ce140a3c7a1387d3c90714d9
yutaabe200 <>
2020-05-25 00:58:38 +0900

    Added project files

---
+++ horizontal-scroll-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  horizontal-scroll-sample
+//
+//  Created by Yuta Abe on 2020/05/25.
+//  Copyright © 2020 Yuta Abe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
